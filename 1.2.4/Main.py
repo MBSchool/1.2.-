66c0bdc24b17927e4652bfc95172fa436d07b7df
MBSchool <zgodspartan@gmail.com>
2024-10-04 11:42:26 -0700

    Changes

--- 1.2.4/Main.py
+++ 1.2.4/Main.py
@@ -9,14 +9,17 @@ wn.setup(width=1.0, height=1.0)
 # --- Setting up walls --- #
 
 maze = trtl.turtle()
-walls = 5
-wall_width = 10
+num_sides = 5
+path_width = 10
 wall_color = "black"
 
+# Maze setup
+
+maze.pensize(5)
+maze.pencolor(wall_color)
+maze.speed("fastest")
+
 # --- Settings --- #
-
-for (x in walls):
-
 
 
 # --- Functions --- #
