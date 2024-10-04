c8234e217f62b3ae6e01e219e7c70b8951de69ea
MBSchool <zgodspartan@gmail.com>
2024-10-04 10:29:01 -0700

    Fixing changes

--- 1.2.2/main.py
+++ 1.2.2/main.py
@@ -1,73 +1,71 @@
-#   a123_apple_1.py
 import turtle as trtl
 import random as rand
+
+# --- Setting up the screen --- #
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
-#-----setup-----
-
-# --- Setting up apple --- #
-
-apple = trtl.Turtle()
-apple_image = "apple.gif" # Store the file name of your shape
+apple_image = "apple.gif"
 wn.addshape(apple_image)
+wn.bgpic("background.gif")
+
+# --- Setting up the apple --- #
+apple = trtl.Turtle()
 apple.penup()
 
-# --- Setting Up list --- #
-
+# --- Setting up letters --- #
 letter_list = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
 letter_list_used = []
-randletter = rand.choice(letter_list)
-currentletter = ""
+current_letter = rand.choice(letter_list)
+randx = rand.randint(-20, 50)
 
-# --- Setting up drawer --- #
-
+# --- Setting up the drawer --- #
 drawer = trtl.Turtle()
 drawer.hideturtle()
 drawer.penup()
 
-# --- Bgpic -- #
-
-wn.bgpic("background.gif")
-
-#-----functions-----
+# --- Functions --- #
 
 def draw_apple(active_apple):
-  active_apple.shape(apple_image)
-  wn.update()
+    active_apple.shape(apple_image)
+    active_apple.showturtle()
+    wn.update()
 
 def move_apple():
- apple.goto(0,-150)
- print("Apple moved to:", apple.ycor())
- reset_apple()
- 
+    apple.goto(randx, -150)
+    print("Apple moved to:", apple.ycor(), apple.xcor())
+    reset_apple()
 
 def draw_random():
-  global randletter
-  drawer.color("Black")
-  drawer.goto(-20,200)
-  drawer.write(f"Press :", {randletter}, font=("Arial", 45, "bold")) 
-
+    global current_letter
+    drawer.clear()
+    drawer.color("black")
+    drawer.goto(-100, 200)
+    drawer.write(f"Press: {current_letter}", font=("Arial", 45, "bold"))
 
 def reset_apple():
-  apple.hideturtle()
-  apple.goto(0,0)
-  apple.showturtle()
-  drawer.clear()
-  draw_random()
-  used_letter_setter()
+    apple.hideturtle()
+    apple.goto(randx, 0)
+    apple.showturtle()
+    set_new_letter()
 
-def used_letter_setter():
-  global randletter
-  letter_list_used.append(randletter)
-  letter_list.pop(randletter)
+def set_new_letter():
+    global current_letter
+    letter_list_used.append(current_letter)
+    letter_list.remove(current_letter)  # Correct removal by value
+    if letter_list:  # Check if there are letters left
+        current_letter = rand.choice(letter_list)
+        draw_random()
+        wn.onkeypress(move_apple, current_letter)
+    else:
+        drawer.clear()
+        drawer.write("You win!", font=("Arial", 45, "bold"))
 
-#-----function calls-----
-  
+# --- Events --- #
+
 draw_apple(apple)
 draw_random()
-print(randletter)
-wn.onkeypress(move_apple, randletter)
-apple.showturtle()
+
+wn.onkeypress(move_apple, current_letter)  # Initial key binding
 wn.listen()
 wn.mainloop()
 
