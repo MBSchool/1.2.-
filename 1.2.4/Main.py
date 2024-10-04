aa800b2212f3d0a2666a72c8f2f6db653cf361f7
MBSchool <zgodspartan@gmail.com>
2024-10-04 11:34:53 -0700

    1.2.4

---
+++ 1.2.4/Main.py
@@ -0,0 +1,32 @@
+import turtle as trtl
+import random as rand
+
+# --- Setting up the screen --- #
+
+wn = trtl.Screen()
+wn.setup(width=1.0, height=1.0)
+
+# --- Setting up walls --- #
+
+maze = trtl.turtle()
+walls = 5
+wall_width = 10
+wall_color = "black"
+
+# --- Settings --- #
+
+for (x in walls):
+
+
+
+# --- Functions --- #
+
+
+
+
+# --- Function calls --- #
+
+
+
+wn.listen()
+wn.mainloop()
